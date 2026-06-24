2b763aa703129cc806705ffbae98551175882dc8
Guillermo Paredes <Guillermo.Paredes@ville.quebec.qc.ca>
2026-06-23 22:51:00 -0400

    correction key sensitive linux

--- portafolio-dotnet-react.client/src/main.jsx
+++ portafolio-dotnet-react.client/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './app.jsx'
+import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter } from "react-router-dom";
-import './stylesheet/app.css'
+import './stylesheet/App.css'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
